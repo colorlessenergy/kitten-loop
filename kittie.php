a235cf37ccf9cd98e73c435368123d8e96ecd46c
colorlessenergy <brianmunozdev@gmail.com>
2016-03-23 18:00:11 -0700

    first commit

---
+++ kittie.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>kittie</title>
+  </head>
+  <body>
+    <?php
+    for($i = 0; $i < 5; $i++) { ?>
+    <img src="kittie<?php $i ?>.jpeg" alt=" kittie picture" /> <?php  
+
+}
+    ?>
+  </body>
+</html>
